6d184cbbc6eebbd20e92c23159b8b14b6533fe8b
korniplodiks <korniplodiks@users.noreply.github.com>
2025-10-21 23:14:07 +0300

    ci: add logging

--- src/api.js
+++ src/api.js
@@ -131,3 +131,30 @@ class API {
 
 // Update 60
 module.exports = API;
+
+
+// API module for PhoneDialer
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 64
+module.exports = API;
