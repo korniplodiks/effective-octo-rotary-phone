77a51117e9a6cdbeb183a0c6c743faa048be4b5c
korniplodiks <korniplodiks@users.noreply.github.com>
2025-12-09 13:55:52 +0200

    remove(db): refactor module structure

--- src/api.js
+++ src/api.js
@@ -23,3 +23,30 @@ class API {
 
 // Update 6
 module.exports = API;
+
+
+// API module for PhoneDialer
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 23
+module.exports = API;
