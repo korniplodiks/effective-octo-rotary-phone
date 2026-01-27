49b1c639e6f6587b2b54aa8a6d7f39b6ddfbde8a
korniplodiks <korniplodiks@users.noreply.github.com>
2026-01-27 17:01:06 +0200

    refactor: performance optimization

--- src/api.js
+++ src/api.js
@@ -104,3 +104,30 @@ class API {
 
 // Update 49
 module.exports = API;
+
+
+// API module for PhoneDialer
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 60
+module.exports = API;
